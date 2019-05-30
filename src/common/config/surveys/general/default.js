2a0cb51e45f372f1c4b62ab60704b75549c3cc34
Richard Frost <richard.frost@headforwards.com>
2019-05-30 14:13:11 +0100

    Updated default survey values and keys

--- src/common/config/surveys/general/default.js
+++ src/common/config/surveys/general/default.js
@@ -1,18 +1,18 @@
 /** ****************************************************************************
  * General survey configuration file.
  **************************************************************************** */
-import $ from 'jquery';
-import Indicia from 'indicia';
-import DateHelp from 'helpers/date';
+import $ from "jquery";
+import Indicia from "indicia";
+import DateHelp from "helpers/date";
 
 const survey = {
-  name: 'default',
-  id: 374,
-  webForm: 'enter-app-record',
+  name: "default",
+  id: 490,
+  webForm: "enter-app-record",
 
   taxonGroups: [], // all
 
-  editForm: ['occ:number', 'occ:stage', 'occ:identifiers'],
+  editForm: ["occ:number", "occ:stage", "occ:identifiers"],
 
   attrs: {
     smp: {
@@ -34,7 +34,7 @@ const survey = {
           $.extend(submission.fields, attributes);
 
           return `${location.latitude}, ${location.longitude}`;
-        },
+        }
       },
       location_accuracy: { id: 282 },
       location_altitude: { id: 283 },
@@ -46,8 +46,8 @@ const survey = {
         id: 273,
         values: {
           iOS: 2398,
-          Android: 2399,
-        },
+          Android: 2399
+        }
       },
 
       device_version: { id: 759 },
@@ -57,70 +57,119 @@ const survey = {
         values(date) {
           return DateHelp.print(date);
         },
-        isValid: val => val && val.toString() !== 'Invalid Date',
-        type: 'date',
-        max: () => new Date(),
+        isValid: val => val && val.toString() !== "Invalid Date",
+        type: "date",
+        max: () => new Date()
       },
 
       activity: {
-        id: 'group',
+        id: "group",
         values: activity => activity.id,
-        type: 'input',
-      },
+        type: "input"
+      }
     },
     occ: {
       training: {
-        id: 'training',
+        id: "training"
       },
 
       taxon: {
         values(taxon) {
           return taxon.warehouse_id;
-        },
+        }
       },
       number: {
-        id: 16,
-        info: 'How many individuals of this type?',
-        label: 'Abundance',
-        icon: 'number',
+        id: 93,
+        info: "How many individuals of this type?",
+        label: "Abundance",
+        icon: "number"
       },
-      'number-ranges': {
-        id: 523,
-        default: 'Present',
+      "number-ranges": {
+        id: 93,
+        default: "Present",
         values: {
           1: 665,
-          '2-5': 666,
-          '6-20': 667,
-          '21-100': 668,
-          '101-500': 669,
-          '500+': 670,
-        },
+          "2-5": 666,
+          "6-20": 667,
+          "21-100": 668,
+          "101-500": 669,
+          "500+": 670
+        }
       },
       stage: {
-        id: 106,
-        info: 'Please pick the life stage.',
-        default: 'Not Recorded',
+        id: 218,
+        info:
+          "Please indicate the life stage and sex of the organism, if recorded.",
+        default: "Not Recorded",
         values: {
-          Adult: 1950,
-          'Pre-adult': 1951,
+          Male: 3484,
+          "Pre-adult": 1951,
+          Female: 3483,
           Other: 1952,
+          Adult: 3405,
+          "Adult male": 3406,
+          "Adult female": 3407,
+          Juvenile: 3408,
+          "Juvenile male": 3409,
+          "Juvenile female": 3410,
+          "Breeding pair": 3411,
+          "Mixed group": 5261,
+          "In flower": 3412,
+          Fruiting: 3413,
+          Egg: 3956,
+          Larva: 3957,
+          Nymph: 3959,
+          Spawn: 3960,
+          Pupa: 3958,
+          "Other (please add to comments)": 3414,
+          "Not recorded": 3415
         },
-        icon: 'stage',
-        type: 'radio',
+        icon: "stage",
+        type: "radio"
+      },
+      type: {
+        id: 217,
+        label: "Select the type of observation that was made.",
+        default: "",
+        values: {
+          Seen: 3383,
+          Heard: 3384,
+          Dead: 3385,
+          "Dead on road": 3386,
+          "Feeding remains": 3387,
+          "Dung or droppings": 3388,
+          "Tracks or trail": 3389,
+          Burrow: 3391,
+          Nest: 3392,
+          Colony: 5667,
+          "Bat Roost": 3924,
+          "Bat Hibernacula": 5294,
+          "Bat Breeding Roost": 5296,
+          "Bat Detected": 5299,
+          "Bat Grounded": 7799,
+          "Bat Seen": 5734,
+          Trap: 5881,
+          "Light trap": 3390,
+          "Net trap": 5869,
+          "Camera trap": 5870,
+          "Other (please add to comments)": 3393
+        },
+        icon: "stage",
+        type: "radio"
       },
       identifiers: {
         id: 18,
         info:
-          'If anyone helped with the identification please enter their name here.',
-        icon: 'user-plus',
-        type: 'text',
+          "If anyone helped with the identification please enter their name here.",
+        icon: "user-plus",
+        type: "text"
       },
       comment: {
-        info: 'Please add any extra info about this record.',
-        icon: 'comment',
-        type: 'text',
-      },
-    },
+        info: "Please add any extra info about this record.",
+        icon: "comment",
+        type: "text"
+      }
+    }
   },
   verify(attrs) {
     const attributes = {};
@@ -135,21 +184,21 @@ const survey = {
     // location
     const location = attrs.location || {};
     if (!location.latitude) {
-      attributes.location = 'missing';
+      attributes.location = "missing";
     }
     // location name
     if (!location.name) {
-      attributes['location name'] = 'missing';
+      attributes["location name"] = "missing";
     }
 
     // date
     if (!attrs.date) {
-      attributes.date = 'missing';
+      attributes.date = "missing";
     } else {
       const date = new Date(attrs.date);
-      if (date === 'Invalid Date' || date > new Date()) {
+      if (date === "Invalid Date" || date > new Date()) {
         attributes.date =
-          new Date(date) > new Date() ? 'future date' : 'invalid';
+          new Date(date) > new Date() ? "future date" : "invalid";
       }
     }
 
@@ -163,7 +212,7 @@ const survey = {
     });
 
     return [attributes, null, occurrences];
-  },
+  }
 };
 
 export default survey;
